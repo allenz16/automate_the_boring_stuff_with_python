a343c81c6535da0a026d7ebcd63139a7ba11d08f
Allen Zhao <mr.zhao.x.i@gmail.com>
2018-10-09 10:44:25 +1100

    updated v1

--- isPhoneNumber_V1.py
+++ isPhoneNumber_V1.py
@@ -1,3 +1,21 @@
-import re
-phoneNumRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
-# this is a test
+def isPhoneNumber(text):
+    if len(text) != 12:
+        return False
+    for i in range(0, 3):
+        if not text[i].isdecimal():
+            return False
+    if text[3] != '-':
+        return False
+    for i in range(4, 7):
+        if not text[i].isdecimal():
+            return False
+    if text[7] != '-':
+        return False
+    for i in range(8, 12):
+        if not text[i].isdecimal():
+            return False
+    return True
+
+
+print(isPhoneNumber('455-896-9966'))
+

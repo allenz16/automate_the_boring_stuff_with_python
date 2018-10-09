4ec1262a0da5ff14ff0c78dabc8eafd107f54ab4
Allen Zhao <mr.zhao.x.i@gmail.com>
2018-10-09 17:32:01 +1100

    create c7_4

---
+++ c7_4_Findall().py
@@ -0,0 +1,10 @@
+import re
+# without groups
+phoneNumRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
+print(phoneNumRegex.findall('home : 489-555-6936, work : 546-889-8877'))
+
+# with groups
+phoneNumRegex1 = re.compile(r'(\d\d\d)-(\d\d\d)-(\d\d\d\d)')
+print(phoneNumRegex1.findall('home : 489-555-6936, work : 546-889-8877'))
+
+#

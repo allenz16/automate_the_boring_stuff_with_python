efd8b6494852be4fae6639ca9afbf65ebfa073a8
Allen Zhao <mr.zhao.x.i@gmail.com>
2018-10-16 13:54:40 +1100

    updated v1

--- c7_8_strong_pass_detection.py
+++ c7_8_strong_pass_detection.py
@@ -1,9 +1,9 @@
 # strong password detection
 import re
+# create password regex
 passRegex = re.compile(r'(?=.*[A-Z])(?=.*[a-z])(?=.*\d).{8,}')
-# capture a password
+# capture a password and verify it
 while True:
-
     password = input('please input your password : ')
     mo1 = passRegex.search(password)
     if mo1 is not None:
@@ -14,7 +14,7 @@ while True:
 print('done')
 
 
-# create password regex
+
 
 
 

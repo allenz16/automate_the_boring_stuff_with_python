38f3e2c248fa1d7056683a23660f5d291c95ace0
Allen Zhao <mr.zhao.x.i@gmail.com>
2018-10-17 17:11:06 +1100

    file created

---
+++ c8_1_path.py
@@ -0,0 +1,32 @@
+import os
+# make a path
+print(os.path.join('user', 'allen', 'document'))
+# make paths
+myFiles = ['accounts.txt', 'details.csv', 'invite.docx']
+for filename in myFiles:
+    print(os.path.join(r'c:\Users\asweigart', filename))
+# get current working directory
+print(os.getcwd())
+# change cwd
+# print(os.chdir('this folder does not exist'))
+# make dir
+# os.makedirs(r'c:\abc\abc')
+# get absolute path
+print(os.path.abspath('.'))
+# check whether the path is an absolute path
+print(os.path.isabs('.'))
+# get relative path between a path and the start
+print(os.path.relpath('c:\\abc\\abc', 'c:\\'))
+calcFilePath = 'c:\\Windows\\System32\\calc.exe'
+# get a path's basename and dirname
+print(os.path.split(calcFilePath))
+# get basename
+print(os.path.basename(calcFilePath))
+# get dirname
+print(os.path.dirname(calcFilePath))
+# get a list of strings from a path
+print(calcFilePath.split(os.path.sep))
+# get single file size
+print(os.path.getsize('c:\\Windows\\System32\\calc.exe'))
+# list folder content
+print(os.listdir('c:\\A'))

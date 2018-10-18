7e7db96acb95c57deaf9e423a4771f5508c31acb
Allen Zhao <mr.zhao.x.i@gmail.com>
2018-10-18 11:17:32 +1100

    new file

---
+++ c8_2_file.py
@@ -0,0 +1,18 @@
+# open a file
+helloFile = open('c:\\A\\wordpress.txt')
+# read a file
+helloContent = helloFile.read()
+print(helloContent)
+# return a list of strings from a file
+aFile = open('c:\\A\\readlines.txt')
+print(aFile.readlines())
+# create a file and write sth to it
+baconFile = open('c:\\A\\bacon.txt', 'w')
+baconFile.write('hello bacon!\n')
+baconFile.close()
+baconFile = open('c:\\A\\bacon.txt', 'a')
+baconFile.write('bacon is not a vegetable.')
+baconFile.close()
+baconFile = open('c:\\A\\bacon.txt')
+content = baconFile.read()
+print(content)

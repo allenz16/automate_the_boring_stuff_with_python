cba3903a959d22b3954b45b423b145a7f34a8edc
Allen Zhao <mr.zhao.x.i@gmail.com>
2018-10-22 17:38:01 +1100

    some updates

--- c7_7_practise.py
+++ c7_7_practise.py
@@ -2,41 +2,45 @@ import re
 # matching 1,230 3 numbers with a comma
 reGex = re.compile(r'^\d{1,3}(,\d{3})*$')
 mo1 = reGex.search('4')
-print(mo1.group())
+#print(mo1.group())
 
 # matching a name with the last name as Nakamoto
 nameRegex = re.compile(r'[A-Z][a-z]+\sNakamoto$')
 mo2 = nameRegex.search('Haha Nakamoto')
-print(mo2.group())
+#print(mo2.group())
 
 # matching a sentence where the first word is either Alice, Bob, or Carol; the second word is either eats, pets, or throws; the third word is apples, cats, or baseballs; and the sentence ends with a period? This regex should be case-insensitive.
 randomRegex = re.compile(r'(Alice|Bob|Carol)\s(eats|pets|throws)\s(apples|cats|baseballs)\.', re.I)
 mo3 = randomRegex.search('Alice eats apples.')
-print(mo3.group())
+#print(mo3.group())
 
 # test
 testRegex = re.compile(r'(\d\d\d)-(\d\d\d-\d\d\d\d)')
 mo = testRegex.findall('445-558-2693 and 456-556-5255')
-print(mo)
+#print(mo)
 
 tRegex = re.compile(r'alice|bob|carol')
 mo4 = tRegex.search('alice and bob')
-print(mo4.group())
+#print(mo4.group())
 
 
 import random
 capitals = {'Alabama': 'Montgomery', 'Alaska': 'Juneau', 'Arizona': 'Phoenix', 'Arkansas': 'Little Rock', 'California': 'Sacramento'}
 states = list(capitals.keys())
-print(capitals.values())
+print('capital.values() is : ', capitals.values())
 #print(capitals.keys())
-print(states)
+print('states is : ', states)
 for questionNum in range(0, 2):
     correctAnswer = capitals[states[questionNum]]
-    print(correctAnswer)
-    #print(capitals.values())
+    print('correctAnswer is : ', correctAnswer)
     wrongAnswers = list(capitals.values())
+    #print('wrongAnswers are : ', wrongAnswers)
     del wrongAnswers[wrongAnswers.index(correctAnswer)]
-    #wrongAnswers = random.sample(wrongAnswers, 3)
-    print(wrongAnswers)
-
+    #print('wrongAnswers are : ', wrongAnswers)
+    wrongAnswers = random.sample(wrongAnswers, 3)
+    #print('wrongAnswers are : ', wrongAnswers)
+    answerOptions = wrongAnswers + [correctAnswer]
+    #print('answerOptions are : ', answerOptions)
+    random.shuffle(answerOptions)
+    #print('answerOptions are : ', answerOptions)
 

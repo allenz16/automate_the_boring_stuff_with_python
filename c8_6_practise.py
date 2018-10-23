8b8f3eb57ce6cdf404f1ca8bd17a93819f39ffed
Allen Zhao <mr.zhao.x.i@gmail.com>
2018-10-23 17:25:39 +1100

    new files.

--- c8_6_practise.py
+++ c8_6_practise.py
@@ -1,4 +1,6 @@
 import random
+import sys
+import pyperclip
 capitals = {'Alabama': 'Montgomery', 'Alaska': 'Juneau', 'Arizona': 'Phoenix', 'Arkansas': 'Little Rock', 'California': 'Sacramento'}
 states = list(capitals.keys())
 print('capital.values() is : ', capitals.values())
@@ -17,3 +19,12 @@ for questionNum in range(0, 2):
     #print('answerOptions are : ', answerOptions)
     random.shuffle(answerOptions)
     print('answerOptions are : ', answerOptions)
+
+# Project MultiClipboard test.
+print(sys.argv)
+dd = pyperclip.paste()
+print(dd)
+
+# How to use dictionary
+testDict = {'allen': '485', 'david': '565'}
+print(str(list(testDict.keys())))

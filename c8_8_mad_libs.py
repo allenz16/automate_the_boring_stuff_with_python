4e12ea8967557537c4f0d4c194b7f7911b98b7f0
Allen Zhao <mr.zhao.x.i@gmail.com>
2018-10-25 17:29:03 +1100

    new file

--- c8_8_mad_libs.py
+++ c8_8_mad_libs.py
@@ -1,7 +1,29 @@
-# Open the file
-aFile = open(r'c:\\A\aFile.txt')
-# Read the file
-content = aFile.readlines()
-# Check the content
-print(content)
+bFile = open('c:\\A\\bFile.txt', 'r+')
+
+while True:
+    line = bFile.readline()
+    if len(line) == 0:
+        break
+    if 'a' in line:
+        sub = input('Type sth to replace \'a\' : ')
+        line = line.replace('a', sub)
+        bFile.writelines(line)
+    if 'b' in line:
+        sub = input('Type sth to replace \'b\' : ')
+        line = line.replace('b', sub)
+        bFile.writelines(line)
+    if 'c' in line:
+        sub = input('Type sth to replace \'c\' : ')
+        line = line.replace('c', sub)
+        bFile.writelines(line)
+bFile.close()
+bFile = open('c:\\A\\bFile.txt')
+print(bFile.readlines())
+
+
+
+
+
+
+
 

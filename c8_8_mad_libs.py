ed13c361e2884fb39ec8c585b61d616f141c6b65
Allen Zhao <mr.zhao.x.i@gmail.com>
2018-10-26 15:08:04 +1100

    final version

--- c8_8_mad_libs.py
+++ c8_8_mad_libs.py
@@ -1,24 +1,17 @@
-bFile = open('c:\\A\\bFile.txt', 'r+')
-
+bFile = open('c:\\A\\bFile.txt', 'r')
+cFile = open('c:\\A\\cFile.txt', 'w')
 while True:
     line = bFile.readline()
     if len(line) == 0:
         break
-    if 'a' in line:
-        sub = input('Type sth to replace \'a\' : ')
-        line = line.replace('a', sub)
-        bFile.writelines(line)
-    if 'b' in line:
-        sub = input('Type sth to replace \'b\' : ')
-        line = line.replace('b', sub)
-        bFile.writelines(line)
-    if 'c' in line:
-        sub = input('Type sth to replace \'c\' : ')
-        line = line.replace('c', sub)
-        bFile.writelines(line)
-bFile.close()
-bFile = open('c:\\A\\bFile.txt')
-print(bFile.readlines())
+    line = line.replace('a', 'A')
+    line = line.replace('b', 'B')
+    cFile.write(line)
+cFile.close()
+cFile = open('c:\\A\\cFile.txt')
+print(cFile.readlines())
+
+
 
 
 

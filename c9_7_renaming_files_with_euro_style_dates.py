5d1a0fae9aba320f709699e8ae17882145001219
Allen Zhao <mr.zhao.x.i@gmail.com>
2018-10-31 17:29:04 +1100

    updated

--- c9_7_renaming_files_with_euro_style_dates.py
+++ c9_7_renaming_files_with_euro_style_dates.py
@@ -5,7 +5,7 @@ datePattern = re.compile(r'''
     ^(.*?)           # all text before the date
     ((0|1)?\d)-      # one or two digits for the month     
     ((0|1|2|3)?\d)-  # one or two digits for the day
-    ((19|20)\d\d)-   # four digits for the year
+    ((19|20)\d\d)   # four digits for the year
     (.*?)$           # all text after the date 
 ''', re.VERBOSE)
 # Loop over the files in the working directory.
@@ -16,7 +16,7 @@ for amerFilename in os.listdir('c:\\A'):
     if mo == None:
         continue
     # Get the different parts of the filename.
-    print(mo)
+    print(mo.group)
     beforePart = mo.group(1)
     monthPart = mo.group(2)
     dayPart = mo.group(4)

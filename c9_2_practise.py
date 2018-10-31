aae4e744804e719e0e5b0d72ec60cdd2fc1ac337
Allen Zhao <mr.zhao.x.i@gmail.com>
2018-10-31 17:29:18 +1100

    updated v1

--- c9_2_practise.py
+++ c9_2_practise.py
@@ -1,4 +1,10 @@
 import re
-testRegex = re.compile(r'.*?')
-mo = testRegex.findall('abc')
-print('mo = ', mo)
+datePattern = re.compile(r'''
+    ^(.*)           # all text before the date
+    ((0|1)?\d)-      # one or two digits for the month     
+    ((0|1|2|3)?\d)-  # one or two digits for the day
+    ((19|20)\d\d)   # four digits for the year
+    (.*)$           # all text after the date 
+''', re.VERBOSE)
+mo = datePattern.search('11-26-2016')
+print(mo.group())

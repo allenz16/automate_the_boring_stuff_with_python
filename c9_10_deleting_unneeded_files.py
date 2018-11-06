8ab3f32426ce52edcaf8e548374209c60d25040b
Allen Zhao <mr.zhao.x.i@gmail.com>
2018-11-06 14:38:55 +1100

    final update

--- c9_10_deleting_unneeded_files.py
+++ c9_10_deleting_unneeded_files.py
@@ -1,8 +1,14 @@
 import os
+import send2trash
 
 
 def delete_big_files(folder):
     for folderName, subFolders, fileNames in os.walk(folder):
         for fileName in fileNames:
-            if os.path.getsize(folderName + os.sep + fileName) > 104857600:
+            if os.path.getsize(folderName + os.sep + fileName) > 5242880:
+                send2trash.send2trash(folderName + os.sep + fileName)
+
+
+delete_big_files('c:\\C')
+
 
